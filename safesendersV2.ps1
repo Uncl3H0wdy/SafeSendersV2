e2b31f7b45620b9d7490f92b758eebe2c0c6bca4
Uncl3H0wdy <github@ybbmail.com>
2024-10-03 12:20:41 +1300

    first commit

---
+++ safesendersV2.ps1
@@ -0,0 +1,53 @@
+Connect-ExchangeOnline
+
+function ValidateMailBox {
+    param ([string] $mbxToValidate)
+    $timer = 0
+    while($true){
+        try{
+            Get-EXOMailbox -Identity $mbxToValidate -ErrorAction Stop
+            Write-Host "The mailbox has been validated" -ForegroundColor Green
+            break
+        }catch{  
+            Write-Error "Could not find the mailbox"
+            break           
+        }
+    }
+}
+$target
+$trustedSender
+while($true){
+    $target = read-host "Type the target users email address"
+
+    try{
+        Write-Host "Validating target mailbox"
+        Get-Mailbox $target -ErrorAction Stop
+        Write-Host "$target has been validated" -ForegroundColor Green
+        break
+    }catch{
+        Write-Host "$target does not exist please enter a valid mailbox" -ForegroundColor Red
+    }
+}
+
+
+$All = Get-Mailbox $target
+
+while($true){
+    $trustedSender = read-host "Type the 'trusted senders' email address"
+    try{
+        $All | ForEach-Object {Set-MailboxJunkEmailConfiguration $_.Name -TrustedSendersAndDomains @{Add="$trustedSender"}}
+        Write-Host "Successfully added $trustedSender to $target's Safe Senders List" -ForeGroundColor Green
+    }catch{
+        "An error occurred."
+        break
+    }
+    
+    $test = (Get-MailboxJunkEmailConfiguration -Identity $target).TrustedSendersAndDomains
+    Write-Host "Here are the addresses in $target's SafeSenders list"
+    foreach($item in $test){
+        Write-Host $item
+    }
+
+    $continue = Read-Host "Would you like to add another user to" $target "'s safesenders list? [Y/N]"
+    if($continue -eq "N"){break}
+}
