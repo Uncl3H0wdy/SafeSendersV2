89b4dfe17e65be71cb3bce7823994011e1bc9578
Uncl3H0wdy <github@ybbmail.com>
2024-10-03 12:30:09 +1300

    Add comments and info

--- safesendersV2.ps1
+++ safesendersV2.ps1
@@ -1,7 +1,11 @@
+# This script is usefull for adding individual users to a targets SafeSenders (TrustedUsersAndDomains)
+
 Connect-ExchangeOnline
 
 $target
 $trustedSender
+
+#Validates that the mailbox exists and loops uptil a valid on is entered
 while($true){
     $target = read-host "Type the target users email address"
 
@@ -17,6 +21,7 @@ while($true){
 
 $All = Get-Mailbox $target
 
+# Add the specified email to the targets safe senders list
 while($true){
     $trustedSender = read-host "Type the 'trusted senders' email address"
     try{
@@ -27,12 +32,14 @@ while($true){
         break
     }
     
+    # List the targets Safe Senders list for volidation
     $test = (Get-MailboxJunkEmailConfiguration -Identity $target).TrustedSendersAndDomains
     Write-Host "Here are the addresses in $target's SafeSenders list"
     foreach($item in $test){
         Write-Host $item
     }
 
+    # Check if another mailbox needs to be added to the SafeSenders list of the target and loop. If not break out of the loop and exit the script
     $continue = Read-Host "Would you like to add another user to" $target "'s safesenders list? [Y/N]"
     if($continue -eq "N"){break}
 }
